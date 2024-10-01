3a3a523ccfb3c5d403519793e1948d4d8468aef0
dibya.borah <dibyajyotiborah469@gmail.com>
2024-10-01 18:36:41 +0530

    done lecture 1 of recursion and backtracking

--- example1.js
+++ example1.js
@@ -1,9 +1,19 @@
 // Recursion is when a fc keeps calling itself until a base condition is met.
 // function keep calling itself to solve problem
 
+// function printNto1(n){
+//     console.log(n);
+//     return (n > 1) && printNto1(n-1);
+// }
+
+
+// Note: If no base case of return statement is written in a recursive fc than stack overflow or infinite loop will be encountered
 function printNto1(n){
+    if(n === 0){
+        return;
+    }
     console.log(n);
-    return (n > 1) && printNto1(n-1);
+    printNto1(n-1);
 }
 
 printNto1(5);
