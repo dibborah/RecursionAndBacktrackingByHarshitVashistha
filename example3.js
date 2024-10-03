f04fc491a5f39bfb621d380c8644e531153cad3b
dibya.borah <dibyajyotiborah469@gmail.com>
2024-10-03 12:42:36 +0530

    done factorial problem with less code . More efficient soln applied

--- example3.js
+++ example3.js
@@ -1,13 +1,24 @@
 // 4! = 4 * 3 * 2 * 1
 
-fact = 1;
-function factorial(n) {
-    fact = fact * n;
-    if (n === 1) {
-        console.log(fact);
-        return;
-    };
-    factorial(n - 1);
-}
+// 1st way
 
-factorial(4);
+// fact = 1;
+// function factorial(n) {
+//     fact = fact * n;
+//     if (n === 1 ) {
+//         console.log(fact);
+//         return;
+//     };
+//     factorial(n - 1);
+// }
+
+// factorial(4);
+
+
+function factorial(n) {
+    if( n === 1) return 1;
+    return n * (factorial(n - 1));
+};
+
+const result = factorial(4);
+console.log(result);
