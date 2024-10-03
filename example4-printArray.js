05f3164b5a01e18e546fdc724fb58a075a6cf325
dibya.borah <dibyajyotiborah469@gmail.com>
2024-10-03 20:40:16 +0530

    modified solution and file name convension

--- example4-printArray.js
+++ example4-printArray.js
@@ -1,5 +1,5 @@
 function printArray(array, index = 0) {
-    if (index >= array.length) return;
+    if (index === array.length) return; // base case
     console.log(array[index]);
     printArray(array, index + 1);
 }
