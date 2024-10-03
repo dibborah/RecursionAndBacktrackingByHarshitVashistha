65823fec0e0263a8cca96844dadcc42d15b99af6
dibya.borah <dibyajyotiborah469@gmail.com>
2024-10-03 11:48:27 +0530

    convered many ways or code structure in which oneToN problem can be solved

--- example2.js
+++ example2.js
@@ -1,12 +1,47 @@
-let i = 1;
-function oneToN(n) {
-    if(i > n){
+// 1st way
+
+// let i = 1;
+// function oneToN(n) {
+//     if(i > n){
+//         return;
+//     }
+//     // console.log((n + i) - n);
+//     console.log(i);
+//     i++;
+//     return oneToN(n);
+// }
+
+// oneToN(5);
+
+// 2nd way
+// function oneToN(n, i) {
+//     if (i > n) {
+//         return;
+//     }
+//     console.log(i);
+//     oneToN(n, i + 1);// in parameter ++ increment operator doesnot works
+// }
+
+// oneToN(5, 1);
+
+// 3rd way
+// function oneToN(n, i = 1) {
+//     if (i > n) {
+//         return;
+//     }
+//     console.log(i);
+//     oneToN(n, i + 1);// in parameter ++ increment operator doesnot works
+// }
+
+// oneToN(5);
+
+// Most important and complex way
+function oneToN(n){
+    if(n === 0){
         return;
     }
-    // console.log((n + i) - n);
-    console.log(i);
-    i++;
-    return oneToN(n);
+    oneToN(n-1);
+    console.log(n);
 }
 
 oneToN(5);
