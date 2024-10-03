ec539dcf9fa5aca7604424ed9933d0747c5a228a
dibya.borah <dibyajyotiborah469@gmail.com>
2024-10-03 20:45:04 +0530

    slightly learned space and time complexity

--- example4-printArray.js
+++ example4-printArray.js
@@ -4,4 +4,10 @@ function printArray(array, index = 0) {
     printArray(array, index + 1);
 }
 
+// function n times called 
+// 1 * O(n)
+
+// note: in recursion internally stack is used
+// temporary space => auxillary space => O(n)
+
 printArray([2, 3, 1, 5, 4]);
