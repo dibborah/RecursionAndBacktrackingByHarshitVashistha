5bb322398daf58e4c1b335e53dd22a51a277804c
dibya.borah <dibyajyotiborah469@gmail.com>
2024-10-05 11:40:32 +0530

    problem solution refactored

--- example7-fibonacci.js
+++ example7-fibonacci.js
@@ -6,9 +6,10 @@
 
 // When you design an algorithm first try to fit the algorithm for 1,2 and 3 case than try to make it a general one
 function fibonacci(n) {
-    if(n === 0) return 0;
-    if(n === 1) return 1;
+    console.log(n);
+    if(n <= 1) return n;
     return fibonacci(n-1) + fibonacci(n-2) // that much times recursion till the get the indexed sum
 }
-
-console.log(fibonacci(7));
+ 
+// console.log(fibonacci(5));
+fibonacci(5);
