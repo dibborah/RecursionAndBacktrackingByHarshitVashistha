c947f42e8a4a7df19893e13e1a1b7e9205b06bf8
dibya.borah <dibyajyotiborah469@gmail.com>
2024-10-05 11:08:15 +0530

    done fibonacci using recursion

---
+++ example7-fibonacci.js
@@ -0,0 +1,14 @@
+// 0 1 1 2 3 5 8 13 ...
+// fib(0) : 0
+// fib(1) : 1
+// fib(2): 1
+// fib(3): 3
+
+// When you design an algorithm first try to fit the algorithm for 1,2 and 3 case than try to make it a general one
+function fibonacci(n) {
+    if(n === 0) return 0;
+    if(n === 1) return 1;
+    return fibonacci(n-1) + fibonacci(n-2) // that much times recursion till the get the indexed sum
+}
+
+console.log(fibonacci(7));
