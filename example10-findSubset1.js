6875604aed912f238ee1df4870a19f173bbfcc24
dibya.borah <dibyajyotiborah469@gmail.com>
2024-10-09 23:00:13 +0530

    done understanding subset recursion leetcode problem

--- example10-findSubset1.js
+++ example10-findSubset1.js
@@ -1,20 +1,37 @@
-function findSubset1(array, outputArray, tempArray = [], index = 0) {
-    if (index >= array.length){
-        outputArray.push([...tempArray]);
-        return;        
-    }
+// function findSubset(array, outputArray, tempArray = [], index = 0) {
+//     if (index >= array.length) {
+//         outputArray.push([...tempArray]);
+//         return;
+//     }
 
-    // 2 conditions
+//     tempArray.push(array[index]);
+//     findSubset(array, outputArray, tempArray, index + 1);
 
-    // 1st include
-    tempArray.push(array[index]);
-    findSubset1(array, outputArray, tempArray, index + 1);
-    tempArray.pop();
+//     tempArray.pop();
+//     findSubset(array, outputArray, tempArray, index + 1);
+// };
+// const outputArray = []
+// findSubset([1,2], outputArray);
+// console.log(outputArray);
 
-    // 2nd not includes
-    findSubset1(array, outputArray, tempArray, index + 1);
-};
+const subset = function (nums) {
+    function backTrack(nums, outputArray, tempArray = [], index = 0) {
+        if (index >= nums.length) {
+            outputArray.push([...tempArray]);
+            return;
+        }
 
-const outputArray = []
-findSubset1([1,2], outputArray);
-console.log(outputArray);
+        tempArray.push(nums[index]);
+        backTrack(nums, outputArray, tempArray, index + 1);
+
+        tempArray.pop();
+        backTrack(nums, outputArray, tempArray, index + 1);
+    };
+
+    const outputArray = []
+    backTrack(nums, outputArray);
+    return outputArray;
+}
+
+const result = subset(["a","b"]);
+console.log(result);
