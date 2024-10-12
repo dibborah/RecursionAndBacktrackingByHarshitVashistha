ce95f9f2df3838c1c268d7323c55f0b86f806590
dibya.borah <dibyajyotiborah469@gmail.com>
2024-10-12 11:52:30 +0530

    starting to practice subset approach 1 again

--- example10-findSubset1.js
+++ example10-findSubset1.js
@@ -35,4 +35,23 @@
 
 // const result = subset(["a","b"]);
 // console.log(result);
+
+function findSubset1(array, outputArray, tempArray = [], index = 0) {
+    if (index === array.length) {
+        outputArray.push([...tempArray]);
+        return;
+    }
+
+    tempArray.push(array[index]);
+    findSubset1(array, outputArray, tempArray, index + 1);
+    tempArray.pop();
+
+    findSubset1(array, outputArray, tempArray, index + 1);    
+}
+
+const outputArray = [];
+findSubset1([1, 2], outputArray);
+
+console.log(outputArray);
+
  
