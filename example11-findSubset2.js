3877970d6904bc015fc50da30717fb0ba6fc26ff
dibya.borah <dibyajyotiborah469@gmail.com>
2024-10-12 14:16:44 +0530

    struture the solution as per leetcode

--- example11-findSubset2.js
+++ example11-findSubset2.js
@@ -3,18 +3,24 @@
 // Lecture 11
 
 // Need to understand the code
-function findSubset2 (array, outputArray, tempArray = [], startIdx = 0) {
-    outputArray.push([...tempArray]);
-    for(let i = startIdx; i < array.length; i++) {
-        tempArray.push(array[i])
-        findSubset2(array, outputArray, tempArray, i + 1);
-        tempArray.pop();
-    }    
+function findSubset2 (array, outputArray) {
+
+    function backTrack (testArray, resultArray, tempArray = [], startIdx = 0) {
+        resultArray.push([...tempArray]);
+        for(let i = startIdx; i < testArray.length; i++) {
+            tempArray.push(testArray[i])
+            backTrack(testArray, resultArray, tempArray, i + 1);
+            tempArray.pop();
+        }    
+    }
+
+    backTrack(array, outputArray);
+    return outputArray;
 }
 
 const outputArray = [];
-const tempArray = [];
-findSubset2([1,2], outputArray, tempArray);
+findSubset2([1,2], outputArray);
+console.log(outputArray);
 
 
 
