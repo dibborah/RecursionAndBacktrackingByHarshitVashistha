e654c36d6eb41295ab994a614bee7f2b78b2cd5b
dibya.borah <dibyajyotiborah469@gmail.com>
2024-10-16 11:52:28 +0530

    done findSubset2 approach 2 but need to understand

--- example13-findSubset2.js
+++ example13-findSubset2.js
@@ -1,26 +1,24 @@
-// Tut way
-
-// Lecture 11
-
-// Need to understand the code
-function findSubset1 (array, outputArray) {
+function findSubset2 (array, outputArray) {
 
     function backTrack (testArray, resultArray, tempArray = [], startIdx = 0) {
         resultArray.push([...tempArray]);
+        //  Need to understand this condition
         for(let i = startIdx; i < testArray.length; i++) {
+            if(i > startIdx && array[i] === array[i - 1]) {
+                continue;
+            }
             tempArray.push(testArray[i])
             backTrack(testArray, resultArray, tempArray, i + 1);
             tempArray.pop();
         }    
     }
 
+    // sorting array
+    array.sort((a, b) => a - b);
     backTrack(array, outputArray);
     return outputArray;
 }
 
 const outputArray = [];
-findSubset2([1,2], outputArray);
+findSubset2([2, 2, 1], outputArray);
 console.log(outputArray);
-
-
-
