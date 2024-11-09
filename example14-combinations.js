051a7370e649870b6be48ca9def58a36504b441b
dibya.borah <dibyajyotiborah469@gmail.com>
2024-11-09 18:24:23 +0530

    combinations question re-practice

--- example14-combinations.js
+++ example14-combinations.js
@@ -1,18 +1,44 @@
-function combinations(n, k) {
+// function combinations(n, k) {
+//     const outputArray = [];
+//     function backTrack(n, k, outputArray, tempArray = [], start = 1) {
+//         if(k === 0) {
+//             outputArray.push([...tempArray]);
+//             return;
+//         }
+//         for (let i = start; i <= n; i++) {
+//             tempArray.push(i);
+//             backTrack(n, k - 1, outputArray, tempArray, i + 1);
+//             tempArray.pop();
+//         }
+//     }
+//     backTrack(n, k, outputArray);
+//     return outputArray;
+// } 
+
+// console.log(combinations(4, 2));
+
+
+
+
+
+
+
+// var combine = function(n, k) {
+function combine(n, k) {
     const outputArray = [];
-    function backTrack(n, k, outputArray, tempArray = [], start = 1) {
-        if(k === 0) {
+    function backTrack(n, k, outputArray, tempArray = [], start = 1){
+        if(k === 0){
             outputArray.push([...tempArray]);
             return;
         }
-        for (let i = start; i <= n; i++) {
+        for(let i = start; i <= n; i++) {
             tempArray.push(i);
             backTrack(n, k - 1, outputArray, tempArray, i + 1);
             tempArray.pop();
-        }
+        }    
     }
     backTrack(n, k, outputArray);
-    return outputArray;
-} 
+    return outputArray;    
+};
 
-console.log(combinations(4, 2));
+console.log(combine(4, 2));
