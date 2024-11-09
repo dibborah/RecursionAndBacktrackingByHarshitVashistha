4e9381ecd5fbcd323c5b2c348d3bdb217194e78a
dibya.borah <dibyajyotiborah469@gmail.com>
2024-11-09 17:17:05 +0530

    structured combinations problem

--- example14-combinations.js
+++ example14-combinations.js
@@ -1,16 +1,18 @@
-function combinations(n, k, outputArray, tempArray = [], start = 1) {
-    if(k === 0) {
-        outputArray.push([...tempArray]);
-        return;
+function combinations(n, k) {
+    const outputArray = [];
+    function backTrack(n, k, outputArray, tempArray = [], start = 1) {
+        if(k === 0) {
+            outputArray.push([...tempArray]);
+            return;
+        }
+        for (let i = start; i <= n; i++) {
+            tempArray.push(i);
+            backTrack(n, k - 1, outputArray, tempArray, i + 1);
+            tempArray.pop();
+        }
     }
-    for (let i = start; i <= n; i++) {
-        tempArray.push(i);
-        combinations(n, k - 1, outputArray, tempArray, i + 1);
-        tempArray.pop();
-    }
+    backTrack(n, k, outputArray);
+    return outputArray;
 } 
 
-const outputArray = [];
-combinations(4, 2, outputArray);
-
-console.log('outputArray', outputArray);
+console.log(combinations(4, 2));
