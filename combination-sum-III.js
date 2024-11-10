e6aa41cfa89f0c29220e3d92387d1bd5a140e5f7
dibya.borah <dibyajyotiborah469@gmail.com>
2024-11-10 16:17:10 +0530

    combination III solution modified

--- combination-sum-III.js
+++ combination-sum-III.js
@@ -6,10 +6,11 @@ function backTrack(k , n, outputArray, tempArray = [], start = 1) {
     if (n < 0) return;
 
     for(let i = start; i <= 9; i++ ) {
-        if (i > start && i === i - 1) {
-            continue;
-        };
-        if (i > n) break;
+        // The below commented line won't be needed as the candidates are in sequence fr. 1 - 9 and duplicates won't be there
+        // if (i > start && i === i - 1) {
+        //     continue;
+        // };
+        // if (i > n) break;
         tempArray.push(i);
         backTrack(k - 1, n - i, outputArray, tempArray, i + 1);
         tempArray.pop();
