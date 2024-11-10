d90275ef1d5db3299ba3e0d93c82579b844ff895
dibya.borah <dibyajyotiborah469@gmail.com>
2024-11-10 18:37:54 +0530

    modified the permutations solution

--- permutation.js
+++ permutation.js
@@ -1,23 +1,25 @@
-function backTrack(array, outputArray, tempArray = [], startIndex = 0) {
+function backTrack(array, outputArray, tempArray = []) {
     if (tempArray.length === array.length) {
         outputArray.push([...tempArray]);
         return;
     };
-    for(let i = startIndex; i < array.length; i++) {
-        if (tempArray.includes(array[i])) {
-          continue;
-        }
+    for(let i = 0; i < array.length; i++) {
+        if (tempArray.includes(array[i])) continue;
         tempArray.push(array[i]);
-        backTrack(array, outputArray, tempArray, startIndex);
+        backTrack(array, outputArray, tempArray);
         tempArray.pop();
     }
 }
+///
 
 const permute = function(nums) {
     const outputArray = [];
     backTrack(nums, outputArray);
     return outputArray;
 }
+
+// number of permutation if elements are distinct 
+// n unique numbers than n!
 
 const nums = [1, 2, 3];
 const result = permute(nums)
