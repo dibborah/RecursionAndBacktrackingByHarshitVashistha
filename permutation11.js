4dd9fde27e9e3b287b5f749256b8cf865e9933fe
dibya.borah <dibyajyotiborah469@gmail.com>
2024-11-10 23:22:03 +0530

    done permutation II solution

--- permutation11.js
+++ permutation11.js
@@ -1,4 +1,4 @@
-function permutation2(array, outputArray, isUsed = new Array(array.length).fill(false), tempArray = [], startIndex = 0) {
+function backTrack(array, outputArray, isUsed = new Array(array.length).fill(false), tempArray = []) {
     if (tempArray.length === array.length) {
         outputArray.push([...tempArray]);
         // return;
@@ -8,17 +8,23 @@ function permutation2(array, outputArray, isUsed = new Array(array.length).fill(
         // if (tempArray.includes(array[i])) { // For some reason this code won't work on duplicate items array
         //    continue;
         // }
-        if (isUsed[i] || (i > 0 && (array[i] === array[i - 1]))) continue;
+        if (isUsed[i] || (i > 0 && (array[i] === array[i - 1]) && !isUsed[i - 1])) continue;
         // if (isUsed[i]) continue;
         tempArray.push(array[i]);
         isUsed[i] = true;
-        permutation2(array, outputArray, isUsed, tempArray, startIndex + 1);
+        backTrack(array, outputArray, isUsed, tempArray);
         tempArray.pop();
         isUsed[i] = false;
     }
 }
 
-const outputArray = [];
+
+function permute2(nums) {
+    const outputArray = [];
+    nums.sort((a, b) => a - b);
+    backTrack(nums, outputArray);
+    return outputArray;
+}
+
 const nums = [1,1,2];
-permutation2(nums, outputArray);
-console.log(outputArray);
+console.log(permute2(nums));
